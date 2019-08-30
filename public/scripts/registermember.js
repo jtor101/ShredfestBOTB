ee32ba0965468ed8b27c78d49f3c9d96c9b2d064
John Torres <jtorres0802@gmail.com>
2019-08-30 15:58:08 -0400

    Further testing done, all redirects work.

--- public/scripts/registermember.js
+++ public/scripts/registermember.js
@@ -6,9 +6,15 @@ $(function() {
 
   $("#submitBtn").on("click", function() {
     confirm("Are you sure you want to add this member?");
-    $.post("api/teams/" + id + "/members", $("#registerMember").serialize());
-    alert("Member added!");
-    location.href = "teamdetails.html?teamid=" + id;
+    $.post(
+      "api/teams/" + id + "/members",
+      $("#registerMember").serialize(),
+      function() {
+        alert("Member added!");
+        location.href = "teamdetails.html?teamid=" + id;
+      }
+    );
+
     //redirects after submit to courses.html
     //return false;
   });
